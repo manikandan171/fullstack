27d6f38b6608c1ce1be7fb38fac7378451c5596d
manikandan <112394806+manikandan171@users.noreply.github.com>
2025-06-25 14:34:31 +0530

    Delete Backend directory

--- Backend/routes/grocery.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { generateGroceryList } from '../controllers/groceryController.js';
-import { authMiddleware } from '../middleware/authMiddleware.js';
-const router = express.Router();
-
-router.get('/', authMiddleware, generateGroceryList);
-
-export default router; 
