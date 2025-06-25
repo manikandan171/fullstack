b4a880cc87bc543be3be70b8f40543648465d12e
manikandan171 <manikandansaran742@gmail.com>
2025-06-25 11:16:07 +0530

    eighth commit

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,9 +2,8 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
-    
-    const mongoURI = 'mongodb+srv://manikandans22msc:1234567890@cluster0.amrtggi.mongodb.net/sample_mflix';
-    await mongoose.connect(mongoURI);
+  
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection failed:', error.message);
